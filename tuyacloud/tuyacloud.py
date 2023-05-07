3f8252fa19d56895bb8eb8ae85a53e3bb8bc5a7c
Razvan Lazar <grigore.razvan.lazar@gmail.com>
2023-05-07 11:39:40 +0300

    [tuyacloud] Add Tuya Clout IoT API implementation.
    Also contain custom implementation for tuya switches smart devices.

---
+++ tuyacloud/tuyacloud.py
@@ -0,0 +1,305 @@
+import json
+import uuid
+import hmac
+import time
+import hashlib
+import requests
+
+"""
+TuyaCloud is designed as a main class for specific Tuya compatible devices
+(ex: TuyaSwitch) implementing the main methods for each device.
+
+Class main methods are built to connect with Tuya IoT Cloud Platform
+https://iot.tuya.com/
+
+1) init
+    Set the API endpoint (TUYA_ENDPOINTS) based on user selected region and get
+    the access token to be used for communication (note that this expires and
+    has to be refreshed)
+
+2) __create_signature (dunder method)
+    Create the signature for each request perform by a Tuya Device.
+
+3) __create_string_to_sign (dunder method)
+    Create the stringToSign required in signature computation
+
+4) command
+    Send a custom command to a Tuya device using POST request
+
+5) get_devices
+    Return a list with all devices associated with current user.
+
+6) print_devices
+    Print the list returned by get_devices method
+"""
+
+################################################################################
+# Tuya Enpoints
+# https://developer.tuya.com/en/docs/iot/api-request?id=Ka4a8uuo1j4t4)
+################################################################################
+TUYA_ENDPOINTS = {
+    "cn" : "https://openapi.tuyacn.com",
+    "w-us" : "https://openapi.tuyaus.com",
+    "e-us" : "https://openapi-ueaz.tuyaus.com",
+    "eu" : "https://openapi.tuyaeu.com",
+    "w-eu" : "https://openapi-weaz.tuyaeu.com",
+    "in" : "https://openapi.tuyain.com"
+}
+
+################################################################################
+# Empty request body encryption
+# https://developer.tuya.com/en/docs/iot/api-request?id=Ka4a8uuo1j4t4)
+################################################################################
+EMPTY_BODY_ENCRYPTION = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+
+################################################################################
+# Invalid token error code
+# https://developer.tuya.com/en/docs/iot/error-code?id=K989ruxx88swc
+#
+# When this error code is returned by a request, the access token has to be
+# refreshed.
+################################################################################
+INVALID_TOKEN = 1011
+
+class TuyaCloud(object):
+    def __init__(self, client_region=None, client_id=None, client_secret=None, device_id=None):
+        """
+        Connect to Tuya Iot Cloud
+
+        Parameters:
+            client_region   : Region (cn|w-us|e-us|eu|w-eu|in)
+            client_id       : Client id (Cloud > "Project" > Authorization Ket > Access ID/Client ID)
+            client_secret   : Client id (Cloud > "Project" > Authorization Ket > Access Secret/Client Secret)
+            device_id       : Tuya device id (set by particular classes that inherit this class)
+        """
+
+        self.device_id = device_id
+        self.client_id = client_id
+        self.access_token = None
+        self.client_secret = client_secret
+        self.client_region = client_region
+
+        # Region validation
+        if self.client_region not in TUYA_ENDPOINTS:
+            raise ValueError("Invalid value for client region")
+
+        self.endpoint = TUYA_ENDPOINTS[self.client_region]
+
+        # Get access token
+        self.refresh_access_token()
+
+
+    def __create_signature(self, t, stringToSign, refresh_token=False):
+        """
+        Build the request signature.
+        https://developer.tuya.com/en/docs/iot/new-singnature?id=Kbw0q34cs2e5g
+
+        Token management API:
+            str = client_id + t + stringToSign
+            sign = HMAC-SHA256(str, secret).toUpperCase()
+
+        General business API:
+            str = client_id + access_token + t + stringToSign
+            sign = HMAC-SHA256(str, secret).toUpperCase()
+        """
+        if refresh_token:
+            data = self.client_id + t + stringToSign
+        else:
+            data = self.client_id + self.access_token + t + stringToSign
+
+        return hmac.new(
+                        self.client_secret.encode('UTF-8'),
+                        data.encode('UTF-8'),
+                        hashlib.sha256
+                    ).hexdigest().upper()
+
+
+    def __create_string_to_sign(self, method, content, headers, url):
+        """
+        Build the stringToSign for request signature.
+        https://developer.tuya.com/en/docs/iot/new-singnature?id=Kbw0q34cs2e5g
+
+        String stringToSign=
+            HTTPMethod + "\n" +
+            Content-SHA256 + "\n" +
+            Headers + "\n" +
+            URL
+        """
+        content_sha256 = None
+        header_sorted = None
+
+        # Create content
+        if content is None:
+            content_sha256 = EMPTY_BODY_ENCRYPTION
+        else:
+            content_sha256 = hashlib.sha256(content.encode('UTF-8')).hexdigest()
+
+        # Create headers (all request headers involved in signature calculation)
+        headers_sorted = ''.join([f'{key}:{headers[key]}\n'
+                            for key in sorted(headers.keys())])
+
+        return f'{method}\n{content_sha256}\n{headers_sorted}\n{url}'
+
+
+    def command(self, content=None):
+        """
+        Send a command to a Tuya device.
+        https://developer.tuya.com/en/docs/cloud/e2512fb901?id=Kag2yag3tiqn5
+        """
+        _URL = f'/v1.0/iot-03/devices/{self.device_id}/commands'
+        time_now = str(int(time.time() * 1000))
+        COMMAND_URL = f'{self.endpoint}{_URL}'
+
+        # Create signature (use body encryption)
+        signature_headers = {
+            "area_id" : self.area_id,
+            "call_id" : self.call_id
+        }
+        stringToSign = self.__create_string_to_sign(
+                                        method  = "POST",
+                                        content = content,
+                                        headers = signature_headers,
+                                        url     = _URL
+                                    )
+
+        signature = self.__create_signature(t=time_now, stringToSign=stringToSign)
+
+        # Create get devices POST request header
+        headers = {
+            "client_id" : self.client_id,
+            "secret" : self.client_secret,
+            "sign" : signature,
+            "t" : time_now,
+            "access_token" : self.access_token,
+            "sign_method" : "HMAC-SHA256",
+            "Signature-Headers" : "area_id:call_id",
+            "area_id" : self.area_id,
+            "call_id" : self.call_id
+        }
+
+        response = requests.post(COMMAND_URL, headers = headers, data = content)
+        json_response = json.loads(response.content)
+        if json_response['success'] == False:
+            # If token has expired, refresh it
+            errcode = int(json_response['code'])
+            if errcode == INVALID_TOKEN:
+                print("Access token expired! Refresh it!")
+                self.refresh_access_token()
+                return self.command(content=content)
+            else:
+                raise ValueError("Unable to get devices list (%s: %s)" %
+                                (json_response['code'], json_response['msg']))
+
+
+    def refresh_access_token(self):
+        """
+        Get Tuya IoT access token (a signature to verify the identity)
+        https://developer.tuya.com/en/docs/iot/new-singnature?id=Kbw0q34cs2e5g
+        """
+        _URL = "/v1.0/token?grant_type=1"
+        time_now = str(int(time.time() * 1000))
+        ACCESS_TOKEN_URL = f'{self.endpoint}{_URL}'
+
+        # Set area id and call id (used for signature calculation)
+        self.area_id = time_now
+        self.call_id = str(uuid.uuid4())
+
+        # Create signature (use encryption for empty body)
+        signature_headers = {
+            "area_id" : self.area_id,
+            "call_id" : self.call_id
+        }
+        stringToSign = self.__create_string_to_sign(
+                                        method  = "GET",
+                                        content = None,
+                                        headers = signature_headers,
+                                        url     = _URL
+                                    )
+        signature = self.__create_signature(t=time_now,stringToSign=stringToSign,refresh_token=True)
+
+        # Create request headers
+        headers = {
+            "client_id" : self.client_id,
+            "secret" : self.client_secret,
+            "sign" : signature,
+            "t" : time_now,
+            "sign_method" : "HMAC-SHA256",
+            "Signature-Headers" : "area_id:call_id",
+            "area_id" : self.area_id,
+            "call_id" : self.call_id
+        }
+
+        # Send request
+        response = requests.get(ACCESS_TOKEN_URL, headers = headers)
+        json_response = json.loads(response.content)
+        if json_response['success'] == False:
+            raise ValueError("Access token refresh error (%s: %s)" % (json_response['code'], json_response['msg']))
+
+        # Get access token
+        self.access_token = json_response['result']['access_token']
+
+
+    def get_devices(self):
+        """
+        Get Tuya devices for current user.
+        """
+        _URL = "/v1.0/iot-01/associated-users/devices"
+        time_now = str(int(time.time() * 1000))
+        DEVICES_URL = f'{self.endpoint}{_URL}'
+
+        # Create signature (use encryption for empty body)
+        signature_headers = {
+            "area_id" : self.area_id,
+            "call_id" : self.call_id
+        }
+        stringToSign = self.__create_string_to_sign(
+                                        method  = "GET",
+                                        content = None,
+                                        headers = signature_headers,
+                                        url     = _URL
+                                    )
+
+        signature = self.__create_signature(t=time_now, stringToSign=stringToSign)
+
+        # Create request headers
+        headers = {
+            "client_id" : self.client_id,
+            "secret" : self.client_secret,
+            "sign" : signature,
+            "t" : time_now,
+            "access_token" : self.access_token,
+            "sign_method" : "HMAC-SHA256",
+            "Signature-Headers" : "area_id:call_id",
+            "area_id" : self.area_id,
+            "call_id" : self.call_id
+        }
+
+        # Send request
+        response = requests.get(DEVICES_URL, headers = headers)
+        json_response = json.loads(response.content)
+        if json_response['success'] == False:
+            # If token has expired, refresh it
+            errcode = int(json_response['code'])
+            if errcode == INVALID_TOKEN:
+                print("Access token expired! Refresh it!")
+                self.refresh_access_token()
+                return self.get_devices()
+            else:
+                raise ValueError("Unable to get devices list (%s: %s)" %
+                                (json_response['code'], json_response['msg']))
+
+        return json_response['result']['devices']
+
+    def print_devices(self):
+        """
+        Print all devices for current user (get_devices)
+        """
+        device_idx = 1
+        json_devices = self.get_devices()
+
+        print("Devices list:")
+        for device in json_devices:
+            print("\tDevice %d:" % (device_idx))
+            print(device)
+            device_idx += 1
+
---
+++ tuyacloud/tuyaswitch.py
@@ -0,0 +1,82 @@
+import json
+import time
+import hmac
+import hashlib
+import requests
+from tuyacloud import TuyaCloud
+
+"""
+TuyaSwitch is designed to control multi-button smart switches compatible with
+Tuya App.
+
+Class has the following methods:
+
+    1) turn_on
+        Allow turning on multiple switches specified by name in the switch_list
+        input parameter.
+
+    2) turn_off
+        Allow turning off multiple switches specified by name in the switch_list
+        input parameter.
+
+    3) turn_cusom
+        Allow custom actions at once for multiple switches specified by name and
+        vale (action) in the switch_dict input parameter.
+"""
+
+class TuyaSwitch(TuyaCloud):
+    def __init__(self, client_region=None, client_id=None, client_secret=None, device_id=None):
+        # Call constructor for TuyaCloud (to ensure API communication)
+        super().__init__(client_region, client_id, client_secret, device_id)
+
+    def turn_on(self, switch_list=None):
+        """
+        Turn on switch(es).
+
+        Parameters:
+            switch_list  : List with switches names to be turned on.
+
+        Ex:
+            obj.turn_on(['switch_1','switch_2'])
+        """
+
+        if switch_list is None:
+            return
+
+        # Create request body
+        switch_body = {'commands':[{'code': key, 'value': True} for key in switch_list]}
+        super().command(content=json.dumps(switch_body))
+
+    def turn_off(self, switch_list=None):
+        """
+        Turn on switch(es).
+
+        Parameters:
+            switch_list  : List with switches names to be turned off.
+
+        Ex:
+            obj.turn_off(['switch_1','switch_2'])
+        """
+
+        if switch_list is None:
+            return
+
+        # Create request body
+        switch_body = {'commands':[{'code': key, 'value': False} for key in switch_list]}
+        super().command(content=json.dumps(switch_body))
+
+    def turn_custom(self, switch_dict=None):
+        """
+        Turn custom switch.
+
+        Parameters:
+            switch_dict : Dictionary with switch name as key and the action
+                          (True/False) as value.
+        """
+
+        if switch_dict is None:
+            return
+
+        # Create request body
+        switch_body = {'commands':[{'code': key, 'value': value} for key,value in switch_dict.items()]}
+        super().command(content=json.dumps(switch_body))
